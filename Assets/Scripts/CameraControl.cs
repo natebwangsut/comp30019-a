aa54568ea29b2a9543d61797fb05a633793a00e7
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-09-02 20:18:11 +1000

    Clean up and add camera controll

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+	public GameObject terrain;
+
+	private Vector3 offset;
+	
+	private float cameraSpeed = 2.5f;
+	private bool smooth = true;
+	
+	// Use this for initialization
+	void Start()
+	{
+		print("Initialised Camera Control.");
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	// Speed controls
+	public float movementSpeed;
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		movementSpeed = 5;
+		rotationSpeed = movementSpeed * 10;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+		// Camera movement on plane
+		float translation = Input.GetAxis("Vertical") * movementSpeed;
+		float rotation = Input.GetAxis("Horizontal") * movementSpeed;
+		
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		
+		transform.Translate(rotation, 0, translation);
+
+		// Roll is custom created in input manager, be sure to see compatibility
+		float r = rotationSpeed * Input.GetAxis("Roll");
+		float h = rotationSpeed * Input.GetAxis("Mouse X");
+		float v = rotationSpeed * Input.GetAxis("Mouse Y");
+
+		r *= Time.deltaTime;
+		h *= Time.deltaTime;
+		v *= Time.deltaTime;
+		
+		transform.Rotate(v, h, r);
+	}
+}
