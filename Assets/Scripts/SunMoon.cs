ca7b5ed345d2139c9dcab336b3d1c164c6aae918
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-09-03 00:06:23 +1000

    Forgot to add files

---
+++ Assets/Scripts/SunMoon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SunMoon : MonoBehaviour
+{
+    public float speed;
+    
+    // Use this for initialization
+    void Start()
+    {
+        speed = (2*Mathf.PI) * 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Assume x-axis is the of West-East
+        transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
