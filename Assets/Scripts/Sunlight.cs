1be23a24303d45d4812151f545ec3802097cdca1
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-09-02 23:10:39 +1000

    Add day/night cycle

---
+++ Assets/Scripts/Sunlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sunlight : MonoBehaviour
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start()
+	{
+		speed = 10;
+		transform.rotation = Quaternion.Euler(Vector3.zero);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Given x-axis is the of West-East
+		transform.Rotate(speed * Time.deltaTime,0 ,0);
+	}
+}
